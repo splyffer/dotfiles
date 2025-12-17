00d821fa75a32212b5ee3a62d395588d5dc19913
carluad <carluad@proton.me>
2025-12-17 17:56:05 -0300

    update

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,6 +3,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set -U fish_greeting
 
+    alias t task
+
     starship init fish | source
     zoxide init fish | source
 end
